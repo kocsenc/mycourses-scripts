29438b44cd0aed3672b5135f3a10a85f392d2d83
Kocsen <kocsenc@gmail.com>
2015-09-01 13:48:38 -0400

    yes

--- common/MyCourses.py
+++ common/MyCourses.py
@@ -4,13 +4,50 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os.path
+
+USER_CONFIG_FILE_NAME = "../user.config"
 
 
 class MyCourses:
-
     def __init__(self):
+        """
+        Instantiate Selenium driver. All scripts should inherit from this.
+        :return:
+        """
         self.driver = webdriver.Firefox()
-        self.wait = WebDriverWait(d, 10)  # Waiting mechanism
+        self.wait = WebDriverWait(self.driver, 10)  # Waiting mechanism
 
         self.driver.get("http://www.mycourses.rit.edu")
-        input("Please log in and press ENTER")
+
+        if os.path.isfile(USER_CONFIG_FILE_NAME):
+            with open(USER_CONFIG_FILE_NAME) as f:
+                user = f.readlines()
+            self.login(user[0].strip(), user[1].strip())
+        else:
+            print(
+                "Did you know you can have a '%s' file with user info to automatically login?" % USER_CONFIG_FILE_NAME)
+            input("Please log in and press <ENTER>")
+
+    def login(self, user, password):
+        """
+        Logs in given user + password
+        :param user:
+        :param password:
+        """
+        username = self.driver.find_element_by_xpath('//*[@id="login_box"]/form/div[1]/div[2]/input')
+        username.send_keys(user)
+
+        pass_field = self.driver.find_element_by_xpath('//*[@id="login_box"]/form/div[2]/div[2]/input')
+        pass_field.send_keys(password)
+
+        pass_field.submit()
+
+    def terminate(self):
+        """
+        Close driver and finish w/ exit code 0
+        :return:
+        """
+        self.driver.close()
+        print('bye bye!')
+        exit(0)
--- scripts/ReadDiscussions.py
+++ scripts/ReadDiscussions.py
@@ -1,21 +1,51 @@
+from selenium.common.exceptions import NoSuchElementException
+
 __author__ = 'kocsenc'
 
 from common.MyCourses import MyCourses
+from time import sleep
+from random import randint
 
 
 class ReadDiscussions(MyCourses):
     def __init__(self):
-        my_courses = super().__init__()
-        self.d = my_courses.driver
-        self.w = my_courses.wait
+        super().__init__()
+        self.d = self.driver
+        self.w = self.wait
 
     def run(self):
-        self._read_discussions()
+        while input("Navigate to the discussion then press <ENTER> or q to quit") != 'q':
+            self._read_discussions()
+        super().terminate()
 
     def _read_discussions(self):
-        input("Navigate to the discussion then press <ENTER>")
+        """
+        Actually read the discussion posts
+        :return:
+        """
+        threads = self.d.find_element_by_xpath('//*[@id="threadDataList"]/ul').find_elements_by_class_name("d2l-inline")
 
-        threads_lis = self.d.find_element_by_xpath('//*[@id="threadDataList"]/ul').find_elements_by_tag_name("li")
+        for i in range(len(threads)):
+            threads = self.d.find_element_by_xpath('//*[@id="threadDataList"]/ul').find_elements_by_class_name(
+                "d2l-inline")
+            thread = threads[i]
+            title = thread.find_element_by_tag_name('a')
+
+            if title.is_displayed():
+                title.click()
+                try:
+                    text = self.d.find_element_by_xpath('//*[@id="threadContentsPlaceholder"]/div/div[1]/div[1]/p').text
+                except NoSuchElementException:
+                    self.d.back()
+                    continue
+
+                num_words = len(text.split(' '))
+                wpm = randint(200, 250)
+                sleep_time = (num_words * 60) / wpm
+                print("Reading %d words at %d wpm. Approx: %d seconds" % (num_words, wpm, sleep_time))
+                sleep(sleep_time)
+
+                self.d.back()
 
 
 if __name__ == "__main__":
